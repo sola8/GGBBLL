38b6f6c1169f320ad32a6d277674c33c9cf7337f
sola8 <makeresola8@gmail.com>
2021-06-19 00:09:05 -0400

    Added comments

--- scripts/points.py
+++ scripts/points.py
@@ -4,19 +4,23 @@ import urllib.request
 from utils import *
 from settings import *
 
+# GGBBLL export for assignment stats and point calculations
 with urllib.request.urlopen(CURRENT_GGBBLL_EXPORT) as f:
     export = json.loads(f.read().decode('utf-8-sig'))
-        
+
+# GBBL export for team mapping to print out points     
 with urllib.request.urlopen(CURRENT_GBBL_EXPORT) as f:
     gbbl_export = json.loads(f.read().decode('utf-8-sig'))
 
 season = export["gameAttributes"]["season"]
 
+# Create dictionary of all GBBL teams for mapping
 teamDict = dict()
 for team in gbbl_export['teams']:
 	teamTid = team['tid']
 	teamDict[teamTid] = team['region'] + " " + team['name']
 
+# Loop through all players in watchlist to calc and print points
 for player in export["players"]:
     if player["watch"] == True: 
         CAP_CHECK = cap_check(player)

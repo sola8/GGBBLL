873ccf7cdb8255ea87a12d0c70742795d421b567
sola8 <makeresola8@gmail.com>
2021-06-20 01:42:03 -0400

    Cleaned up comments

--- assignments.py
+++ assignments.py
@@ -7,26 +7,26 @@ from settings import *
 allPlayers = []
 
 with urllib.request.urlopen(CURRENT_GBBL_EXPORT) as f:
-    export = json.loads(f.read().decode('utf-8-sig'))
+    GBBL = json.loads(f.read().decode('utf-8-sig'))
 
 with open(CURRENT_LOCAL_GGBBLL_EXPORT, encoding='utf-8-sig') as f:
-   ggbbll_export = json.load(f)
+    GGBBLL = json.load(f)
 
 # Grab all current players from GBBL export
-for player in export['players']:
+for player in GBBL['players']:
     if (player['tid'] != -3 and player['tid'] != -2):
         allPlayers.append(player)
 
 # Get the current GBBL season
-currentYear = export["gameAttributes"]["season"]
+currentYear = GBBL["gameAttributes"]["season"]
 
 with open(ASSIGNMENT_FILE_NAME, "r") as file:
     # Loop through .txt file with assignments in it
     for line in file:
         # Compare names in .txt file with names in export 
-        # If match, take the player's info in export
+        # If match, take the player's info
         player = list(filter(lambda player: (find_player(player)) == line.strip(), allPlayers))[0]
-        # Clear unneeded keys from player
+        # Clear unneeded keys
         for key in list(player.keys()):
             if key not in player_keys: 
                 del player[key]
@@ -38,14 +38,13 @@ with open(ASSIGNMENT_FILE_NAME, "r") as file:
         # Then, put them on the watch list
         player['tid'] = main_to_g_league(player)
         player['watch'] = True
-        ggbbll_export['players'].append(player) # Append to list of total assignments
+        # Append to list of players in GGBBLL export
+        GGBBLL['players'].append(player)
 
-assignFile = CURRENT_LOCAL_GGBBLL_EXPORT.replace(".json", "_updated.json")
+assignFile = CURRENT_LOCAL_GGBBLL_EXPORT.replace(".json", "_assigned.json")
 
-# Dump to assignments.json file to paste into GGBBLL export later
-# Still need to figure out how to put assignments into GGBBLL export without C/P
-# And this doesn't handle players with no last name...
+# Dump to new file with assignments
 with open(assignFile, "w") as o:
     print('Creating export...')
-    json.dump(ggbbll_export, o)
+    json.dump(GGBBLL, o)
     print('Export done.')
